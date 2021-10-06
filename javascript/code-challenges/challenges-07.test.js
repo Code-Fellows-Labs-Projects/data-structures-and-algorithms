973afdf3a5b96685d6888f90b3d91dc3017ba485
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-10-06 08:50:48 -0700

    refactored q11

--- javascript/code-challenges/challenges-07.test.js
+++ javascript/code-challenges/challenges-07.test.js
@@ -231,9 +231,7 @@ Write a function named totalSumCSV that, given a string of comma-separated value
 const totalSumCSV = (str) => {
   let total = 0;
   let splitString = str.split(',');
-  splitString.map(number => {
-    total += parseInt(number);
-  });
+  splitString.map(number => total += parseInt(number));
   return total;
 };
 
