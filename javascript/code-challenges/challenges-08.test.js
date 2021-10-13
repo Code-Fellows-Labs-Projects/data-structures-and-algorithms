4760fd4cda15a64f6238633409dfd3d55361a520
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-10-12 20:49:41 -0700

    codechallenge08 q9 complete

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -178,7 +178,8 @@ For example, 'Welcome to Code 301!' will return 'W_lc_m_ t_ C_d_ 301!'.
 ------------------------------------------------------------------------------------------------ */
 
 let hangman = (str) => {
-  // Solution code here...
+  let regex  = /[aeoiu]/ig;
+  return str.replace(regex,'_');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -314,7 +315,7 @@ describe('Testing challenge 8', () => {
   });
 });
 
-xdescribe('Testing challenge 9', () => {
+describe('Testing challenge 9', () => {
   let startString = 'This is a regex challenge. We are trying to create a hangman phrase where all of the vowels are missing!';
 
   test('It should remove the vowels from the hangman string and replace them with underscores', () => {
