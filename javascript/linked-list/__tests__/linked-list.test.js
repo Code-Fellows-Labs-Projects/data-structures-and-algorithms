c95c2938a89ccf4d3fbc2931ed9b2b3993da4d6a
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-10-23 11:46:39 -0700

    LinkedList class created, methods refactored with error catching

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -8,3 +8,8 @@ describe('Linked List', () => {
     expect(true).toBeTruthy();
   });
 });
+
+// list.head = new Node(10);
+// list.head.next = new Node(25);
+// list.head.next.next = new Node(2);
+// list.head.next.next.next = new Node(13);
